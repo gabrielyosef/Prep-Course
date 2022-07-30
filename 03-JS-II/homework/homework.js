757c280f44cba6e2da37c34c38231a98548d1555
Gabriel <gabriel.yosef@gmail.com>
2022-07-30 16:24:45 -0300

    Homework JSII completo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -203,8 +203,12 @@ switch (valor) {
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-
+  //Escribe tu código aquí
+  let tabla = []   
+for(i = 0; i <= 10; i++){
+tabla.push(6 * i)
+}
+return tabla
 }
 
 function tieneTresDigitos(numero){
